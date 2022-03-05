522eca85ff4ea1f06b3370e022a83d5dcee4d680
Priyansh Sahu <priyansh.sahu25@gmail.com>
2022-03-05 11:21:13 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import News from './News'
 
 import {
-  HashRouter as Router, Route, Switch
+  BrowserRouter as Router, Route, Switch
 
 } from "react-router-dom";
 
